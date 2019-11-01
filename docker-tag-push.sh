63b878728866abc35dd1bbc098bbc8d1b648d47c
vanderw <vanderw@localhost>
2019-11-01 19:13:56 +0100

    aargh

--- docker-tag-push.sh
+++ docker-tag-push.sh
@@ -1,2 +1,2 @@
-docker tag personify/personify-scim-server:1.0.3-RELEASE 172.30.1.1:5000/personify/personify-scim-server:1.0.3-RELEASE
-docker push 172.30.1.1:5000/personify/personify-scim-server:1.0.3-RELEASE
+docker tag personify/personify-scim-server:1.0.4.RELEASE 172.30.1.1:5000/personify/personify-scim-server:1.0.4.RELEASE
+docker push 172.30.1.1:5000/personify/personify-scim-server:1.0.4.RELEASE
