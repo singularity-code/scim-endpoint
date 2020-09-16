78641330f51f459cc63c8aebef707166f2dd45b3
vanderw <wouter@mogo.be>
2020-09-16 21:20:02 +0200

    1.1.3 scim server

--- docker-tag-push.sh
+++ docker-tag-push.sh
@@ -1,3 +1,3 @@
-docker tag personify/personify-scim-server:1.1.2.RELEASE personify/personify-scim-server:latest
-docker push personify/personify-scim-server:1.1.2.RELEASE
+docker tag personify/personify-scim-server:1.1.3.RELEASE personify/personify-scim-server:latest
+docker push personify/personify-scim-server:1.1.3.RELEASE
 docker push personify/personify-scim-server:latest
