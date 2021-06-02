f173ebc0d8a99a8ed81fcdaac13bfc32e92e2b94
Steve Jerman <steve@kloudspot.com>
2021-06-02 13:22:29 -0600

    add put test

--- src/test/java/be/personify/iam/scim/ProvisionUpdateTest.java
+++ src/test/java/be/personify/iam/scim/ProvisionUpdateTest.java
@@ -4,19 +4,14 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import be.personify.iam.scim.init.Application;
-import be.personify.iam.scim.rest.SchemaController;
-import be.personify.iam.scim.schema.Schema;
-import be.personify.iam.scim.schema.SchemaReader;
-import be.personify.iam.scim.storage.StorageImplementationFactory;
-import be.personify.util.StringUtils;
-import com.jayway.jsonpath.JsonPath;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +19,18 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
+import be.personify.iam.scim.init.Application;
+import be.personify.iam.scim.rest.SchemaController;
+import be.personify.iam.scim.schema.Schema;
+import be.personify.iam.scim.schema.SchemaReader;
+import be.personify.iam.scim.storage.StorageImplementationFactory;
+import be.personify.iam.scim.util.Constants;
+import be.personify.util.StringUtils;
 
 @SpringBootTest(classes = Application.class)
 @AutoConfigureMockMvc
@@ -41,6 +48,8 @@ public class ProvisionUpdateTest {
   @Autowired private StorageImplementationFactory storageImplementationFactory;
 
   @Autowired private SchemaReader schemaReader;
+
+  @Autowired private ObjectMapper mapper;
 
   private void clearStorage() {
     Schema schema = schemaReader.getSchema("urn:ietf:params:scim:schemas:core:2.0:Group");
@@ -74,12 +83,51 @@ public class ProvisionUpdateTest {
     String u2 = createUser("/test-reqs/user_create2.json");
 
     patchEntity("user", u2, "/test-reqs/user_patch1.json");
-    System.err.println(getEntity("user", u2));
+    printPayload(getEntity("user", u2));
 
     patchEntity("user", u2, "/test-reqs/user_patch2.json");
-    System.err.println(getEntity("user", u2));
+    printPayload(getEntity("user", u2));
     patchEntity("user", u2, "/test-reqs/user_patch3.json");
-    System.err.println(getEntity("user", u2));
+    printPayload(getEntity("user", u2));
+  }
+
+  @Test
+  public void updateUser() throws UnsupportedEncodingException, IOException, Exception {
+    clearStorage();
+
+    String u2 = createUser("/test-reqs/user_create3.json");
+
+    putEntity("user", u2, "/test-reqs/user_modify3.json");
+    printPayload(getEntity("user", u2));
+  }
+
+  private void putEntity(String entityType, String id, String jsonFile)
+      throws IOException, Exception {
+
+    Map<String, Object> ent = readEntity(getJson(jsonFile));
+    ent.put(Constants.ID, id);
+
+    restMock
+        .perform(
+            put("/scim/v2/" + StringUtils.capitalize(entityType) + "s/" + id)
+                .with(httpBasic("scim-user", "changeit"))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(ent))
+                .accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().is2xxSuccessful())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+  }
+
+  private Map<String, Object> readEntity(String payload)
+      throws JsonMappingException, JsonProcessingException {
+    TypeReference<HashMap<String, Object>> typeRef =
+        new TypeReference<HashMap<String, Object>>() {};
+    return mapper.readValue(payload, typeRef);
+  }
+
+  private void printPayload(String payload) throws JsonMappingException, JsonProcessingException {
+    Map<String, Object> ent = readEntity(payload);
+    System.err.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(ent));
   }
 
   private void patchEntity(String entityType, String id, String jsonFile)
@@ -104,7 +152,7 @@ public class ProvisionUpdateTest {
                 .accept(MediaType.APPLICATION_JSON))
         .andDo(
             r -> {
-              System.err.println("Groups:\n" + r.getResponse().getContentAsString());
+              printPayload(r.getResponse().getContentAsString());
             })
         .andExpect(status().isOk())
         .andExpect(content().contentType(MediaType.APPLICATION_JSON));
@@ -138,7 +186,7 @@ public class ProvisionUpdateTest {
                 .accept(MediaType.APPLICATION_JSON))
         .andDo(
             r -> {
-              System.err.println("Users:\n" + r.getResponse().getContentAsString());
+              printPayload(r.getResponse().getContentAsString());
             })
         .andExpect(status().isOk())
         .andExpect(content().contentType(MediaType.APPLICATION_JSON));
