99c18d5ca11f0c91f1421a8746865d051338cd46
vanderw <wouter@mogo.be>
2022-01-27 18:44:08 +0100

    fix for issue https://bitbucket.org/wouter29/personify-scim-server/issues/22/error-with-ldap-connection-storage

--- docker-tag-push.sh
+++ docker-tag-push.sh
@@ -1,3 +1,3 @@
-docker tag personify/personify-scim-server:1.2.6.RELEASE personify/personify-scim-server:latest
-docker push personify/personify-scim-server:1.2.6.RELEASE
+docker tag personify/personify-scim-server:1.2.7.RELEASE personify/personify-scim-server:latest
+docker push personify/personify-scim-server:1.2.7.RELEASE
 docker push personify/personify-scim-server:latest
