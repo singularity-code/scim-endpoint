2efb29f53f8fe3dc8d63f3d6ca349fdbbd3e8101
vanderw <wouter@mogo.be>
2022-08-31 20:59:24 +0200

    1.4.4

--- docker-tag-push.sh
+++ docker-tag-push.sh
@@ -1,3 +1,3 @@
-docker tag personify/personify-scim-server:1.4.1.RELEASE personify/personify-scim-server:latest
-docker push personify/personify-scim-server:1.4.1.RELEASE
+docker tag personify/personify-scim-server:1.4.4.RELEASE personify/personify-scim-server:latest
+docker push personify/personify-scim-server:1.4.4.RELEASE
 docker push personify/personify-scim-server:latest
